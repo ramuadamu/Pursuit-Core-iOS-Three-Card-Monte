59ec98792a53df1f63bb6399a6ae1e8de7ca86fa
Ramu <ramu@Pursuits-MacBook-Pro-4.local>
2018-11-07 17:55:51 -0500

    message

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -1,20 +1,96 @@
 //
 //  ViewController.swift
-//  ThreeCardMonte
+//  Three Card Monte Project
 //
-//  Created by Alex Paul on 11/5/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
+//  Created by Ramu on 11/7/18.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // outlet
+    @IBOutlet weak var label: UILabel!
+    
+    
+// cardbutton outlet
+    @IBOutlet weak var cardButton1: UIButton!
+    
+    @IBOutlet weak var cardButton2: UIButton!
+    
+    @IBOutlet weak var cardButton3: UIButton!
+    
+    @IBOutlet weak var backgroundViewColor: UIView!
+    @IBOutlet weak var displayLabel: UILabel!
+    
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    
+    @IBAction func cardFlipped(_ sender: UIButton) {
+        var cardArray = [UIImage.init(named: "kingCard"), UIImage.init(named: "threeCard"), UIImage.init(named: "threeCard")]
+        cardArray = cardArray.shuffled()
+        
+        switch sender.tag {
+        case 0:
+            if cardArray[0] == UIImage.init(named:"kingCard") {
+                cardButton1.setImage(UIImage.init(named:"kingCard"), for: .normal)
+                displayLabel.text = "You won"
+
+            } else {
+                cardButton1.setImage(UIImage.init(named:"threeCard"), for: .normal)
+                
+                displayLabel.text = "You lost"
+
+            }
+            cardButton1.isEnabled = false
+            cardButton2.isEnabled = false
+            cardButton3.isEnabled = false
+        case 1:
+            if cardArray[1] == UIImage.init(named:"kingCard") {
+                cardButton2.setImage(UIImage.init(named:"kingCard"), for: .normal)
+                displayLabel.text = "You won 🏆"
+            } else {
+                cardButton2.setImage(UIImage.init(named:"threeCard"), for: .normal)
+                displayLabel.text = "You lost ☠️"
+            }
+            cardButton1.isEnabled = false
+            cardButton2.isEnabled = false
+            cardButton3.isEnabled = false
+        case 2:
+            if cardArray[2] == UIImage.init(named:"kingCard") {
+                cardButton3.setImage(UIImage.init(named:"kingCard"), for: .normal )
+                displayLabel.text = "You won 🏆"
+            } else {
+                cardButton3.setImage(UIImage.init(named:"threeCard"), for: .normal)
+                displayLabel.text = "You lost ☠️"
+            }
+            cardButton1.isEnabled = false
+            cardButton2.isEnabled = false
+            cardButton3.isEnabled = false
+        default:
+            print("invalid Entry")
+    
+    
+    }
+        cardButton1.isEnabled = false
+        cardButton2.isEnabled = false
+        cardButton3.isEnabled = false
+        
+    
+}
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func startGame(_ sender: Any) {
+        cardButton1.isEnabled = true
+        cardButton2.isEnabled = true
+        cardButton3.isEnabled = true
+        
+        cardButton1.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+        cardButton2.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+        cardButton3.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+    }
+
 
 
 }
-
